f878fa51bf42fe347408f32b228319fe63e65d04
CanTinGit <ctxx61@gmail.com>
2017-09-29 20:52:38 +0100

    Init Project and add PlayerController.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += new Vector3(0.0f, speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position += new Vector3(0.0f, -speed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += new Vector3(-speed * Time.deltaTime,0.0f);
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += new Vector3(speed * Time.deltaTime, 0.0f);
+        }
+
+    }
+}
