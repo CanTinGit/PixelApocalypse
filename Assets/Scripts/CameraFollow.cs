008d4306552fefd99077cbf39e580370c30e1229
CanTinGit <ctxx61@gmail.com>
2017-09-29 21:56:05 +0100

    Enemy AI
    Enemy AI and try to add CameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+ //   GameObject player;
+ //   Vector3 playerLastPosition;
+ //   Vector3 moveDistance;
+ //   Vector3 offset = new Vector3(0, 0, -10);
+ //   Vector3 distanceToPlayer;
+ //   public float maxDistance;
+ //   public float maxDistanceToPlayer;
+ //   public float speed;
+ //   float step;
+	//// Use this for initialization
+	//void Awake () {
+ //       player = GameObject.FindGameObjectWithTag("Player");
+	//}
+
+ //   void Start()
+ //   {
+ //       playerLastPosition = player.transform.position;
+ //   }
+	
+	//// Update is called once per frame
+	//void Update ()
+ //   {
+ //       moveDistance = player.transform.position - playerLastPosition;
+ //       step = speed * Time.deltaTime;
+ //       if(moveDistance.magnitude > maxDistance)
+ //       {
+ //           transform.position = Vector3.MoveTowards(transform.position, player.transform.position + offset, step);
+ //           distanceToPlayer = player.transform.position - (transform.position - offset);
+ //           if (distanceToPlayer.magnitude < maxDistanceToPlayer)
+ //               playerLastPosition = player.transform.position;
+ //       }
+
+        
+ //   }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+	
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	// Update is called once per frame
+	void Update () {
+
+        //if (player.transform.position.x > this.transform.position.x)
+        //    gameObject.transform.position += new Vector3(speed * Time.deltaTime, 0.0f);
+        //if (player.transform.position.y > this.transform.position.y)
+        //    gameObject.transform.position += new Vector3(0.0f, speed * Time.deltaTime);
+        //if (player.transform.position.x < this.transform.position.x)
+        //    gameObject.transform.position += new Vector3(-speed * Time.deltaTime, 0.0f);
+        //if (player.transform.position.y < this.transform.position.y)
+        //    gameObject.transform.position += new Vector3(0.0f, -speed * Time.deltaTime);
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+    }
+}
